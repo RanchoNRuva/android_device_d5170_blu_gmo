65dd141c1295f28961a63f48c52a071aeb437846
RanchoNRuva <malandkarved@gmail.com>
2020-01-19 19:31:46 -0500

    mkvendor.sh: Initial commit of BLU_STUDIO_G2

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_BLU_STUDIO_G2.mk
---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := BLU_STUDIO_G2
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/BLU/BLU_STUDIO_G2/device_BLU_STUDIO_G2.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := BLU_STUDIO_G2
+PRODUCT_NAME := cm_BLU_STUDIO_G2
+PRODUCT_BRAND := BLU
+PRODUCT_MODEL := BLU_STUDIO_G2
+PRODUCT_MANUFACTURER := BLU
