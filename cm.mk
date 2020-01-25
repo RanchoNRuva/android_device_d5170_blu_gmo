7361a7374b9a0c78de1979be6b76e7ff3a109544
RanchoNRuva <malandkarved@gmail.com>
2020-01-24 19:44:25 -0500

    First commit

--- cm.mk
+++ cm.mk
@@ -7,6 +7,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/BLU/BLU_STUDIO_G2/device_BLU_STUDIO_G2.mk)
 
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 854
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := BLU_STUDIO_G2
 PRODUCT_NAME := cm_BLU_STUDIO_G2
---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+add_lunch_combo cm_BLU_STUDIO_G2-userdebug
+add_lunch_combo cm_BLU_STUDIO_G2-eng
+
+
